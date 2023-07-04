f967770908873db56b95aaf6d042d8024947a542
guiferreira-git <guisferreira@protonmail.com>
2023-07-04 11:28:54 -0300

    projeto finalizado

---
+++ script.js
@@ -0,0 +1,18 @@
+function toggleMode() {
+  let html = document.documentElement
+  html.classList.toggle('light')
+  // ou:
+// if (html.classList.contains('light')) {
+//    html.classList.remove('light')
+//  } else {
+//    html.classList.add('light')
+//  }
+
+  let img = document.querySelector('#profile img')
+  
+  if (html.classList.contains('light')) {
+    img.setAttribute('src', '/assets/avatar-light.png')
+  } else {
+    img.setAttribute('src', '/assets/avatar.png')
+  }
+}
