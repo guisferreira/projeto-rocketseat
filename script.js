00485a224c2b0aa81cb95c35765dcd55cfb67c08
guiferreira-git <guisferreira@protonmail.com>
2023-07-05 12:48:24 -0300

    img fix

--- script.js
+++ script.js
@@ -11,8 +11,8 @@ function toggleMode() {
   let img = document.querySelector('#profile img')
   
   if (html.classList.contains('light')) {
-    img.setAttribute('src', '/assets/avatar-light.png')
+    img.setAttribute('src', './assets/avatar-light.png')
   } else {
-    img.setAttribute('src', '/assets/avatar.png')
+    img.setAttribute('src', './assets/avatar.png')
   }
 }
